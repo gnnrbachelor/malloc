9ba21d6a6d45d88e28e20a2a3e9daa055b630b4a
root <gnnrbachelor@gmail.com>
2022-06-06 18:34:54 +0000

    Fix typo

--- malloc.h
+++ malloc.h
@@ -27,7 +27,7 @@ typedef struct block_data
 void *naive_malloc(size_t size);
 void *_malloc(size_t size);
 void _free(void *ptr);
-void *_calloc(size_t nmemb, size_t size);{
+void *_calloc(size_t nmemb, size_t size);
 
 
 #endif
